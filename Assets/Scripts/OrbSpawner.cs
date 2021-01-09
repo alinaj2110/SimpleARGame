63bacab55a384568a90301b4c18be66af9f907e3
alinaj2110 <anjalipraveen2110@gmail.com>
2021-01-09 23:48:17 +0530

    Addition of Virtual Buttons to move the Player, Player Movement Script

---
+++ Assets/Scripts/OrbSpawner.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using Vuforia;
+
+public class OrbSpawner : MonoBehaviour
+{
+    public GameObject orb;
+    public GameObject ARAnchor;
+    public float zone_range;
+    public float timedelay;
+    public float repRate;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        InvokeRepeating("OrbSpawn", timedelay, repRate);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (core == null)
+            //CancelInvoke(); //makes the aliens to stop spawning once the core is destroyed
+        
+    }
+
+    //spawn function
+    public void OrbSpawn()
+    {
+        //int index = Random.Range(0, alienPrefabs.Length);
+        //Vector3 spawnPos = new Vector3(Random.Range(-zone_range, zone_range), Random.Range(-zone_range, zone_range), Random.Range(-zone_range, zone_range)); //randZonePoint(zone_radius_min);
+        Instantiate(orb,ARAnchor.transform.position  +Random.insideUnitSphere*10 , transform.rotation);          //transform.position+spawnPos
+    }
+
+}
